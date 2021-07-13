a6dad7f00817757cfb9508fc018db4d72d0cd270
Daniel Roa <danielroapetrasic@gmail.com>
2021-07-13 17:59:10 -0400

    20210713_1

--- src/app/views/nuevo-destinatario/nuevo-destinatario.component.ts
+++ src/app/views/nuevo-destinatario/nuevo-destinatario.component.ts
@@ -1,9 +1,14 @@
 import { Banco, Bank } from './../../models/banco';
-import { Component, OnInit, TestabilityRegistry } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { InputMaskAngularModule } from 'input-mask-angular';
+
 import * as validarRUT from '../../util/validarRUT';
 
+/* import {FormControl} from '@angular/forms'; */
+
 import { ListaBancosService } from '../../services/lista-bancos.service';
+import { MatDialog, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 declare var validarRUT: any;
 
@@ -14,21 +19,26 @@ declare var validarRUT: any;
 })
 export class NuevoDestinatarioComponent implements OnInit {
 
-  title = "Nuevo destinatario"
+  title = "Nuevo destinatario";
   _bancos: string[] = [];
+  _tipoCuentas: string[] = ['Cuenta Corriente', 'Cuenta de Ahorro' , 'Cuenta Vista' , 'Cuenta Rut']
 
   destinatarioForm: FormGroup;
   emailRegx = /^(([^<>+()\[\]\\.,;:\s@"-#$%&=]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/;
+  rutRegx = /^\d{1,2}\.\d{3}\.\d{3}[-][0-9kK]{1}$/
 
   constructor(
     private formBuilder: FormBuilder,
-    private listaBancosService: ListaBancosService
-  ) { }
+    private listaBancosService: ListaBancosService,
+    public dialogo: MatDialog
+  ) { 
+
+  }
 
   ngOnInit() {
     this.destinatarioForm = this.formBuilder.group({
       nombre: [null, Validators.required],
-      rut: [null, Validators.required],
+      rut: [null, [Validators.required, Validators.pattern(this.rutRegx)]],
       email: [null, [Validators.required, Validators.pattern(this.emailRegx)]],
       telefono: [null, Validators.required],
       banco: [null, Validators.required],
@@ -38,15 +48,19 @@ export class NuevoDestinatarioComponent implements OnInit {
 
     this.getBancos();
 
-    console.log('Lista de bancos =>', this._bancos);
-
   }
 
   submit() {
-    if (!this.destinatarioForm.valid) {
+    if (this.destinatarioForm.valid) {
+
+      console.log(this.destinatarioForm.value);
+
+      this.destinatarioForm.reset();
       return;
     }
-    console.log(this.destinatarioForm.value);
+
+    
+    
   }
 
   getBancos(){
@@ -59,16 +73,52 @@ export class NuevoDestinatarioComponent implements OnInit {
     });
   }
 
+  mostrarDialogo(): void {
+
+    this.dialogo
+      .open(DialogoConfirmacionComponent, {
+        data: `¿Confirma a ${ this.destinatarioForm.value.nombre } como nuevo destinatario para transferencias? `  
+      })
+      .afterClosed()
+      .subscribe((confirmado: Boolean) => {
+        if (confirmado) {      
+          alert("¡Destinatario agregado!");
+          this.submit();
+        } else {
+          alert("Cancelado");
+        }
+      });
+  }
+
   //https://palabrasalcierre.wordpress.com/2009/11/24/formateo-y-validacion-de-runjava-script/
 
 }
 
-/* 
-• Nombre
-• RUT
-• Correo
-• Número de teléfono
-• Banco de Destino
-• Tipo de cuenta
-• Número de cuenta
- */
+@Component({
+  selector: 'dialog-confirmacion',
+  template: `
+<h1 mat-dialog-title>Confirmación</h1>
+<div mat-dialog-content>
+    <p>{{ mensaje }}</p>
+</div>
+<div mat-dialog-actions>
+    <button mat-raised-button color="primary" style="margin: 10px" (click)="cerrarDialogo()">No</button>
+    <button mat-raised-button color="primary" style="margin: 10px" (click)="confirmado()" cdkFocusInitial>Sí</button>
+</div>
+  `
+})
+export class DialogoConfirmacionComponent {
+
+  constructor(
+    public dialogo: MatDialogRef<DialogoConfirmacionComponent>,
+    @Inject(MAT_DIALOG_DATA) public mensaje: string) { }
+
+  cerrarDialogo(): void {
+    this.dialogo.close(false);
+  }
+  confirmado(): void {
+    this.dialogo.close(true);
+  }
+
+
+}
